2ea88a7eaed0596d871d0522f91f98ef45f0d757
vir2s <virtualik@gmail.com>
2021-04-14 18:50:18 +0300

    class-based API views added

--- courses_api/urls.py
+++ courses_api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from courses_api.views import courses_list, course_detail
+from courses_api.views import courses_list, course_detail, CourseAPIView, CourseDetailsAPIView
 
 urlpatterns = [
-    path('courses/', courses_list),
-    path('course/<int:pk>/', course_detail),
+    # path('courses/', courses_list),
+    path('courses/', CourseAPIView.as_view()),
+    # path('course/<int:pk>/', course_detail),
+    path('course/<int:id>/', CourseDetailsAPIView.as_view()),
 ]
--- courses_api/views.py
+++ courses_api/views.py
@@ -7,8 +7,57 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 
+# Class-Based Views
+class CourseAPIView(APIView):
+
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CourseDetailsAPIView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Course.objects.get(id=id)
+        except Course.DoesNotExist:
+            return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        course = self.get_object(id)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        course = self.get_object(id)
+        serializer = CourseSerializer(course, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        course = self.get_object(id)
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Functional Views
 @api_view(['GET', 'POST'])
 def courses_list(request):
 

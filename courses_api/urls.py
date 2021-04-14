994a057a551ad92911bf25b009d49958f3c415ce
vir2s <virtualik@gmail.com>
2021-04-14 21:31:54 +0300

    generic API views and mixins added

--- courses_api/urls.py
+++ courses_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from courses_api.views import courses_list, course_detail, CourseAPIView, CourseDetailsAPIView
+from courses_api.views import courses_list, course_detail, CourseAPIView, CourseDetailsAPIView, GenericAPIView
 
 urlpatterns = [
     # path('courses/', courses_list),
     path('courses/', CourseAPIView.as_view()),
     # path('course/<int:pk>/', course_detail),
     path('course/<int:id>/', CourseDetailsAPIView.as_view()),
+    path('generic/course/<int:id>/', GenericAPIView.as_view()),
 ]
--- courses_api/views.py
+++ courses_api/views.py
@@ -8,6 +8,36 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import generics, mixins
+
+
+# Generic API Views
+class GenericAPIView(generics.GenericAPIView,
+                     mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin):
+
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request, id)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 
 # Class-Based Views
